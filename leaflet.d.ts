dea3e25a68f5fc38744f6370bc1b340707d1193e
no <154220919+Mehedi6898@users.noreply.github.com>
2026-02-09 16:59:37 -0600

    Delete leaflet.d.ts

--- leaflet.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "react-leaflet" {
-  import { ComponentType } from "react";
-  import { MapContainerProps } from "react-leaflet";
-
-  export const MapContainer: ComponentType<MapContainerProps & { center?: [number, number] }>;
-}
